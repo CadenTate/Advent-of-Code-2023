bce31ec8112b87a81b7b624507a4d431ece7800d
Caden Tate <=>
2023-12-08 11:24:47 -0600

    Test Case Passes

--- Day2.py
+++ Day2.py
@@ -1,6 +1,41 @@
 game = {}
 
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# Game = Whole Line
+# Toss = One Section (divided by semicolon)
+# Cube = Number plus color in format [num,color]
+
+IDSum = 0
+
+rMax = 12
+gMax = 13
+bMax = 14
+
 with open("test.txt") as file:
-    line = str.split(file.readline(),",")
-    line = [i.split(",") for i in line]
-    print(line)
+    for game in file:
+        ID = int(game[5])
+        game = game[8:]
+        game = game.split(";")
+        addID = True
+
+        for toss in game:
+            r = 0
+            g = 0
+            b = 0
+            toss = toss.split(",")
+            for cube in toss:
+                cube = cube.strip().split(" ")
+                match cube[1]:
+                    case "red":
+                        r += int(cube[0])
+                    case "green":
+                        g += int(cube[0])
+                    case "blue":
+                        b += int(cube[0])
+            if r > rMax or b > bMax or g > gMax:
+                addID = False
+                break
+        if addID:
+            IDSum += ID
+
+print(IDSum)
