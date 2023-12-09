1250776c2a8753fdbe0fe2a1370b424991f4a470
CadenTate <cadentate0511@gmail.com>
2023-12-08 20:19:51 -0600

    Complete

--- Day 2/Day2 Part 2.py
+++ Day 2/Day2 Part 2.py
@@ -1,40 +1,36 @@
-game = {}
-
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 # Game = Whole Line
 # Toss = One Section (divided by semicolon)
 # Cube = Number plus color in format [num,color]
 
-IDSum = 0
+power = 0
 
-rMax = 12
-gMax = 13
-bMax = 14
-
-with open("text.txt") as file:
+with open(r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2023\Day 2\inputDay2.txt") as file:
     for game in file:
-        ID = int(game.split(":")[0].split()[1])
-        game = game.split(";")
-        addID = True
+        # Converting file to format used 
+        game = game.split(":")
+        game.pop(0)
+        game = game[0].split(";")
+
+        r = 0
+        g = 0
+        b = 0
 
         for toss in game:
-            r = 0
-            g = 0
-            b = 0
             toss = toss.split(",")
             for cube in toss:
                 cube = cube.strip().split(" ")
+                cubeNum = int(cube[0])
                 match cube[1]:
                     case "red":
-                        r += int(cube[0])
+                        if cubeNum > r:
+                            r = cubeNum
                     case "green":
-                        g += int(cube[0])
+                        if cubeNum > g:
+                            g = cubeNum
                     case "blue":
-                        b += int(cube[0])
-            if r > rMax or b > bMax or g > gMax:
-                addID = False
-                break
-        if addID:
-            IDSum += ID
+                        if cubeNum > b:
+                            b = cubeNum
+        power += r * g * b
 
-print(IDSum)
+print(power)
