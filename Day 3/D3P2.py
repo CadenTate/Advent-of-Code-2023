50bd2d7548ea78f5667f678e10ea2581db84a591
Caden Tate <=>
2023-12-15 10:00:45 -0600

    Day 3 Complete

--- Day 3/D3P2.py
+++ Day 3/D3P2.py
@@ -4,9 +4,17 @@ def isNum(num) -> bool:
         return True
     except:
         return False
+    
+def checkData(key:int,isDataNum:bool=False) -> None:
+    if isDataNum:
+        if key not in dataNums:
+            dataNums[key] = []
+    else: 
+        if key not in data:
+            data[key] = 0
 
 # Get File Setup
-prefix = r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2023\Day 3"
+prefix = r"C:\Users\ctate0455\Desktop\Code\Advent-of-Code-2023\Day 3"
 filename = "inputDay3.txt"
 
 # Get File
@@ -26,8 +34,9 @@ for line in file:
     text += line.strip()
 
 # Logic
-sum = 0
 num = ""
+data = {}
+dataNums = {}
 
 # Iterate Through Each Character
 for i,char in enumerate(text):
@@ -38,48 +47,90 @@ for i,char in enumerate(text):
         if num != "":
             for f,digit in enumerate(num):
                 # Check up and down first since all digits of num need to do that
-                if i - len(num) - length + f > 0 and text[i-len(num)-length+f] != ".":
-                    sum += int(num)
+
+                # Up
+                digitIndex = i-len(num)-length+f
+                if digitIndex > 0 and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num) 
                     print("NUM:",num,"DIGIT:",digit,"UP")
                     break
-                if i - len(num) + length + f < len(text) and text[i-len(num)+length+f] != ".":
-                    sum += int(num)
+
+                # Down
+                digitIndex = i - len(num) + length + f
+                if digitIndex < len(text) and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num) 
                     print("NUM:",num,"DIGIT:",digit,"down")
                     break
 
                 # Check 45 degrees
                 # Up Left
-                if i - len(num) + f - length + 1 > 0 and text[i-len(num)+f-length + 1] != ".":
-                    sum += int(num)
+                digitIndex = i - len(num) + f - length + 1
+                if  digitIndex > 0 and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num) 
                     print("NUM:",num,"DIGIT:",digit,"UP left")
                     break
                 # Down Left
-                if i - len(num) + length - 1 + f < len(text) and text[i-len(num)+length - 1+f] != ".":
-                    sum += int(num)
+                digitIndex = i - len(num) + length - 1 + f
+                if digitIndex < len(text) and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num) 
                     print("NUM:",num,"DIGIT:",digit,"Dwon Left")
                     break
                 # Up Right
-                if i - len(num) - length - 1 + f > 0 and text[i-len(num)-length - 1+f] != ".":
-                    sum += int(num)
+                digitIndex = i -len(num) - length - 1 + f
+                if digitIndex > 0 and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num) 
                     print("NUM:",num,"DIGIT:",digit,"UP Right")
                     break
                 # Down Right
-                if i - len(num) + length + 1 + f < len(text) and text[i-len(num)+length + 1+f] != ".":
-                    sum += int(num)
+                digitIndex = i - len(num) + length + 1 + f
+                if digitIndex < len(text) and text[digitIndex] == "*":
+                    checkData(digitIndex)
+                    data[digitIndex] += 1
+                    checkData(digitIndex,True)
+                    dataNums[digitIndex].append(num)
                     print("NUM:",num,"DIGIT:",digit,"Down Right")
                     break
 
             # Unique Cases for first and last
             # First Digit Case
-            if i - len(num) - 1 > -1 and text[i - len(num) - 1] != ".":
-                sum += int(num)
+            digitIndex = i - len(num) - 1 
+            if digitIndex > -1 and text[digitIndex] == "*":
+                checkData(digitIndex)
+                data[digitIndex] += 1
+                checkData(digitIndex,True)
+                dataNums[digitIndex].append(num) 
                 print("NUM:",num,"DIGIT:",num[0],"Left")
                 # Last Digit
-            if text[i] != ".":
+            digitIndex = i
+            if text[digitIndex] == "*":
+                checkData(digitIndex)
+                data[digitIndex] += 1
+                checkData(digitIndex,True)
+                dataNums[digitIndex].append(num) 
                 print("NUM:",num,"DIGIT:",num[-1],"Right")
-                sum += int(num)
 
         # Reset num
         num = ""  
+
+sum = 0
+
+for cog in data:
+    if data[cog] == 2:
+        sum += int(dataNums[cog][0]) * int(dataNums[cog][1]) 
 
 print(sum)
