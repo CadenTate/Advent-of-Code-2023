4cfbf4b124a5c713c7146bacf891d59ad1bbabf8
CadenTate <cadentate0511@gmail.com>
2024-03-22 00:31:32 -0500

    halved time taken to complete

--- Day 6/D6.py
+++ Day 6/D6.py
@@ -1,4 +1,6 @@
-with open(r"C:\Users\ctate0455\Desktop\Advent-of-Code-2023\Day 6\input.txt") as file:
+import math
+
+with open(r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2023\Day 6\input.txt") as file:
     file = file.readlines()
 
 times = file[0].split()[1:]
@@ -9,11 +11,13 @@ winAmount = 1
 for i in range(len(times)):
     time = int(times[i])
     total = 0
-    for holdTime in range(time):
+    for holdTime in range(math.ceil(time/2)):
         moveTime = time - holdTime
         distanceMoved = moveTime * holdTime
         if distanceMoved > int(distance[i]):
             total += 1
+    total *= 2
+    if time % 2 == 0: total += 1
     winAmount *= total
 
 print(winAmount)
