05dbfffa4f1e05b6a92b2ce0566b645187c17c8f
Caden Tate <cadentate0511@gmail.com>
2024-03-22 13:30:21 -0500

    SUPER FAST VERSION

--- Day 6/D6.py
+++ Day 6/D6.py
@@ -1,6 +1,9 @@
 import math
 
-with open(r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2023\Day 6\input.txt") as file:
+def quadratic(a,b,c):
+    return (-b - math.sqrt(b*b-4*a*c)) / 2 * a
+
+with open(r"C:\Users\ctate0455\Desktop\Advent-of-Code-2023\Day 6\input.txt") as file:
     file = file.readlines()
 
 times = file[0].split()[1:]
@@ -8,16 +11,16 @@ distance = file[1].split()[1:]
 
 winAmount = 1
 
-for i in range(len(times)):
-    time = int(times[i])
-    total = 0
-    for holdTime in range(math.ceil(time/2)):
-        moveTime = time - holdTime
-        distanceMoved = moveTime * holdTime
-        if distanceMoved > int(distance[i]):
-            total += 1
-    total *= 2
-    if time % 2 == 0: total += 1
-    winAmount *= total
+for time, distance in zip(times, distance):
+    n = int(time) + 1
+    distance = int(distance)
+
+    minIndex = math.floor(quadratic(1,-(n-1),distance))
+
+    badTimes = 2 * (minIndex + 1)
+    answer = n - badTimes
+
+    print(f"Debug:\nN = {n}\nDistance = {distance}\nMin Index = {minIndex}\nBad Times = {badTimes}\nAnswer = {answer}\n")
+    winAmount *= answer
 
 print(winAmount)
