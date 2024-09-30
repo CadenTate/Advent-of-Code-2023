6ef10af07177cb7b384d763ee61e2c8d46818ba7
Caden Tate <cadentate0511@gmail.com>
2024-09-30 16:20:14 -0500

    D7 Recode

--- Day 7/D7.py
+++ Day 7/D7.py
@@ -1,37 +1,27 @@
-path = r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2023\Day 7\input.txt"
+path = r"C:\Users\Intern\Desktop\Advent-of-Code-2023\Day 7\input.txt"
 
 with open(path) as file:
-    file = file.readlines()
+    file = {file.strip().split()[0]:file.strip().split()[1] for file in file.readlines()}
 
-file = [item.split() for item in file]
+print(file)
 
-score = {}
+def same(card:str):
+    oldValue = card[0]
+    for value in card:
+        if oldValue != value:
+            print("FAILED")
+            return(False)
+        oldValue = value
+    print("PASSED")
+    return(True)
 
-for item in file:
-    card = {}
-    for letter in item[0]:
-        if letter not in card:
-            card[letter] = 1
-        else:
-            card[letter] += 1
-    
-    values = [value for value in card.values()]
+five = []
+four = []
+full = []
+three = []
+twoPair = []
+onePair = []
+highCard = []
 
-    print(values)
-
-    if 5 in values:
-        score[item[1]] = 7
-    elif 4 in values:
-        score[item[1]] = 6
-    elif 3 in values and 2 in values:
-        score[item[1]] = 5
-    elif values.count(3) == 1 and values.count(1) == 2:
-        score[item[1]] = 4
-    elif values.count(1) == 2 or values.count(2) == 2:
-        score[item[1]] = 3
-    elif 2 in values:
-        score[item[1]] = 2
-    else:
-        score[item[1]] = 1
-        
-print(score)
+for card in file:
+    same(card)
